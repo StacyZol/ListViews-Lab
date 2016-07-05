bbfdcc8e08259c192c8d96f21b29ffdd5e0e72fa
StacyZol <stacyzolnikov@Stacy-Zolnikovs-MacBook-Pro.local>
2016-07-05 09:04:25 -0400

    Tried to complete it

--- starter-code/Bookshelf/app/src/main/java/ly/generalassemb/drewmahrt/bookshelf/MainActivity.java
+++ starter-code/Bookshelf/app/src/main/java/ly/generalassemb/drewmahrt/bookshelf/MainActivity.java
@@ -1,11 +1,13 @@
 package ly.generalassemb.drewmahrt.bookshelf;
 
 import android.content.Context;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -18,10 +20,9 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     BaseAdapter mBookAdapter;
+    ListView mListView;
+    List<Book> mBookList = new ArrayList<Book>();
 
-    //TODO: Define your ListView
-
-    //TODO: Define your Book List
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,34 +30,63 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Use helper method to add books to the list
-        mBookList = generateBooks();
+       mBookList = generateBooks();
 
-        //TODO: Instantiate BaseAdapter
+        mListView = (ListView)findViewById(R.id.listview);
+
+        // Instantiate BaseAdapter
         //Below is a partially complete example for one Adapter
         mBookAdapter = new BaseAdapter() {
-            @Override
-            public int getCount() {
+            mBookAdapter = new ArrayAdapter<Book>(this, android.R.layout.simple_list_item_2, android.R.id.text1, mBookList)
+
+            {
+
+
+                @Override
+                public int getCount () {
                 return mBookList.size();
             }
 
-            @Override
-            public Object getItem(int position) {
+                @Override
+                public Object getItem ( int position){
                 return mBookList.get(position);
             }
 
-            @Override
-            public long getItemId(int position) {
+                @Override
+                public long getItemId ( int position){
                 return position;
             }
 
-            @Override
-            public View getView(int position, View convertView, ViewGroup parent) {
-                //TODO: Update the view
+                @Override
+                public View getView ( int position, View convertView, ViewGroup parent){
+                // Update the view
+                View view = convertView;
+                if (view == null) {
+                    LayoutInflater inflater = (LayoutInflater) MainActivity.this.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                    view = inflater.inflate(android.R.layout.simple_list_item_1, null);
+                }
+                final TextView title = (TextView) view.findViewById(android.R.id.text1);
+                final TextView author = (TextView) view.findViewById(android.R.id.text2);
+
+                title.setText(mBookList.get(position).getTitle());
+                author.setText(mBookList.get(position).getAuthor());
+
+                view.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View view) {
+                        title.setTextColor(ContextCompat.getColor(view.getContext(), R.color.colorAccent));
+                        author.setTextColor(ContextCompat.getColor(view.getContext(), R.color.colorAccent));
+                    }
+                });
+                return view;
             }
+            }
+
         };
 
-        //TODO: Set the ListView's adapter
-
+        // Set the ListView's adapter
+        mListView.setAdapter(mBookAdapter);
     }
 
     //Method to generate a list of Books
